2b848bbb1da0724c463b7be5f608785fe873989a
whiteyear <chanyeung96@gmail.com>
2026-06-16 22:45:34 +0800

    added relationships

--- mini-accounting-system/app/Models/JournalEntry.php
+++ mini-accounting-system/app/Models/JournalEntry.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class JournalEntry extends Model
 {
-    //
+    public function journalEntryLines()
+    {
+        return $this->hasMany(JournalEntryLine::class);
+    }
 }
--- mini-accounting-system/app/Models/JournalEntryLine.php
+++ mini-accounting-system/app/Models/JournalEntryLine.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class JournalEntryLine extends Model
 {
-    //
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function journalEntry()
+    {
+        return $this->belongsTo(JournalEntry::class);
+    }
 }
